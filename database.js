f681065e384fe3d43075e365cd4abe0210037ff3
obedabit <obitbit30@gmail.com>
2023-05-03 01:42:46 +0300

    connect to database using uri

--- database.js
+++ database.js
@@ -1,13 +1,5 @@
 const mysql = require("mysql2");
 
-const pool = new mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  timezone: "+03:00",
-  dateStrings: "DATETIME",
-  port: process.env.PORT,
-}).promise();
+const pool = new mysql.createPool(process.env.DATABASE_URL).promise();
 
 module.exports = pool;
